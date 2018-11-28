baa7477eef471f4a9a832ab378b3fcced323eff2
Artyom Sog <tomas.tomson@mail.ru>
2018-11-28 22:43:54 +0300

    Kotlin Android Tutorial - Best way to update Recycler View with DiffUtils Fin

--- app/src/main/java/com/artyom/sogbatyan/edmtdiffutilsrecyclerview/MainActivity.kt
+++ app/src/main/java/com/artyom/sogbatyan/edmtdiffutilsrecyclerview/MainActivity.kt
@@ -2,11 +2,49 @@ package com.artyom.sogbatyan.edmtdiffutilsrecyclerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import com.artyom.sogbatyan.edmtdiffutilsrecyclerview.Util.MyAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    internal var dataSource: MutableList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_view.setHasFixedSize(true)
+        recycler_view.layoutManager = (LinearLayoutManager(this))
+
+        initData()
+
+        val adapter = MyAdapter(dataSource)
+        recycler_view.adapter = adapter
+
+        btn_insert.setOnClickListener {
+            val newData = ArrayList<String>()
+
+            for (i in 0..2)
+                newData.add(UUID.randomUUID().toString())
+            adapter.insertItem(newData)
+            recycler_view.smoothScrollToPosition(adapter.itemCount-1)
+        }
+
+        btn_update.setOnClickListener {
+            val newData = ArrayList<String>()
+
+            for (i in 0..2)
+                newData.add(UUID.randomUUID().toString())
+            adapter.insertItem(newData)
+        }
+    }
+
+    private fun initData() {
+        for (i in 0..1) {
+            dataSource.add(UUID.randomUUID().toString())
+        }
     }
 }
---
+++ app/src/main/java/com/artyom/sogbatyan/edmtdiffutilsrecyclerview/Util/MyAdapter.kt
@@ -0,0 +1,48 @@
+package com.artyom.sogbatyan.edmtdiffutilsrecyclerview.Util
+
+import android.support.v7.util.DiffUtil
+import com.artyom.sogbatyan.edmtdiffutilsrecyclerview.R
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class MyAdapter(private val dataSource: MutableList<String>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): MyViewHolder {
+        val itemView: View = LayoutInflater.from(p0.context)
+                .inflate(R.layout.item_layout, p0, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return dataSource.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.textView.text = dataSource.get(position)
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal var textView: TextView = itemView.findViewById(R.id.txt_text) as TextView
+
+    }
+
+    fun insertItem(newList: List<String>) {
+        val diffUtilCallback = MyDiffUtilCallback(dataSource, newList)
+        val diffResult: DiffUtil.DiffResult = DiffUtil.calculateDiff(diffUtilCallback)
+
+        dataSource.addAll(newList)
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    fun updateItem(newList: List<String>) {
+        val diffUtilCallback = MyDiffUtilCallback(dataSource, newList)
+        val diffResult: DiffUtil.DiffResult = DiffUtil.calculateDiff(diffUtilCallback)
+
+        dataSource.clear()
+        dataSource.addAll(newList)
+        diffResult.dispatchUpdatesTo(this)
+    }
+}
